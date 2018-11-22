3f176e0bb51bd885cf44131a2ed9bc2920a9f99d
Wendler Zacariotto <zwendler@amazon.com>
2018-11-22 08:15:32 +0000

    Updating script to add metrics update in cloudwatch metrics, also removing Alerts, now user can create alerts based on the metrics. Now it run to all the region except to ap-northeast-3 due to security reason, that region require other plocies

--- LambdaHandler/LambdaVersionMemoryChecker.py
+++ LambdaHandler/LambdaVersionMemoryChecker.py
@@ -1,47 +1,64 @@
 from __future__ import absolute_import, print_function, unicode_literals
 import logging
+import datetime;
 import boto3
-
-# JSON Event example
-# {
-#   "topic_arn": "arn:aws:sns:eu-west-1:123456789:S3notification",
-#   "notification": false,
-#   "read_only": true,
-#   "aws_region": "eu-west-1",
-#   "num_versions": 0
-# }
 
 # Initialize logger and set log level
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# Initialize Lambda and SNS client for the specified region
+# Initialize Lambda and CloudWatch client for the specific region
 def initialize_services(region):
     session = boto3.Session(region_name=region)
-    return [session.client('lambda'), session.client('sns')]
+    return [session.client('lambda'), session.client('cloudwatch')]
 
 # Validate the events
 def validate_event(event):
-    if 'aws_region' not in event:
-        logger.info('Region not informed, will execute in us-east-1 (N.Virginia)')
-        event['aws_region'] = 'us-east-1'
-    if 'notification' not in event:
-        logger.info('Notification not informed, no notification will be send')
-        event['notification'] = True
     if 'num_versions' not in event:
         logger.info('Number of Version not informed, function will check only the latest versions')
         event['num_versions'] = 1
     if 'read_only' not in event:
         logger.info('Read Only not informed, function will only read')
         event['read_only'] = True
-    if 'notification' in event and event['notification'] and 'topic_arn' not in event:
-        logger.error('Notification allowed but topic not informed')
-        return 'Notification allowed but topic not informed'
-    if 'topic_arn' in event and event['aws_region'] not in event['topic_arn']:
-        logger.error('Topic region is different from the region informed')
-        return 'Topic region is different from the region informed'
-    return None
 
+# Read all the verions from the Functions in that region
+# versions = versions_all[1:-5]
+# This would change list from:
+# ["Latest", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# To:
+# [1, 2, 3, 4, 5, 6, 7, 8]
+def read_versions_from_funtion(function, num_version, client_lambda, region):
+    memory_free = 0
+    versions_all = client_lambda.list_versions_by_function(FunctionName=function['FunctionArn'])['Versions']
+    versions = versions_all[1:-int(num_version)]
+
+    if not len(versions): logger.info('{}: No versions older than the specified value for FunctionName={}'.format(region, function['FunctionArn']))
+    for version in versions:
+        if version['Version'] != function['Version']:
+            arn = version['FunctionArn']
+            logger.info('{}: FunctionName={}'.format(arn))
+            logger.info('{}: Memory size of this version: {} MB\n'.format(region, version['MemorySize']))
+
+            if not event['read_only']: memory_free += remove_version(version, client_lambda)
+        else:
+            logger.info("{}: Ignoring Latest version for function: {}".format(region, function['Version']))
+    return memory_free
+
+
+# Create CloudWatch Metric for Code Storage usage
+def put_metrics_cloudwatch(client_cloudwatch, in_use_memory):
+    client_cloudwatch.put_metric_data(
+        Namespace='AWS/Lambda',
+        MetricData=[
+            {
+                'MetricName': 'UsedCodeStorage',
+                'Timestamp': datetime.datetime.now().timestamp(),
+                'Value': in_use_memory/1024,
+                'Unit': 'Gigabytes',
+                'StorageResolution': 60
+            },
+        ]
+    )
 
 # Delete the version and return the total of memory freed
 def remove_version(version, client):
@@ -54,61 +71,24 @@ def remove_version(version, client):
         logger.error('Failed to delete: ' + str(e))
         return 0
 
-
-# Send a notification to the Topic input
-def alert_memory_usage(memory_usage, topic_arn, client_sns):
-
-    # Send message
-    try:
-        response = client_sns.publish(
-            TopicArn=topic_arn,
-            Message="The total size, in bytes, of the account's deployment packages per region is close to the limit. You are using " + memory_usage + "of 76800MB"
-        )
-        logger.info('Notification Sent to the following topic {}'.format(topic_arn))
-    except:
-        logger.error('Failed top delete: ' + str(e))
-
-
 def clean_old_lambda_versions(event, context):
-    validation = validate_event(event)
-    if bool(validation):
-        raise Exception(validation)
-    client = initialize_services(event['aws_region'])
-    functions = client[0].list_functions()['Functions']
-    memory_usage = int(client[0].get_account_settings()['AccountUsage']['TotalCodeSize'])
-    # Check if memory usage is above 75% of the maximum for that account in that region.
-    # Check if the notifications are allowed.
-    if memory_usage >= int(client[0].get_account_settings()['AccountLimit']['TotalCodeSize']) * 0.75 and event['notification']:
-        alert_memory_usage(str(int(memory_usage/1024)), event['topic_arn'], client[1])
-
-    if len(functions) != 0:
+    validate_event(event)
+    message = []
+    ec2_regions = [region['RegionName'] for region in boto3.client('ec2').describe_regions()['Regions']]
+    for region in ec2_regions:
+        if region == 'ap-northeast-3': continue
+        client = initialize_services(region)
+        functions = client[0].list_functions()['Functions']
+        memory_usage = int(client[0].get_account_settings()['AccountUsage']['TotalCodeSize'])
         memory_free = 0
-        logger.info('Listing Functions Versioned')
-        for function in functions:
-
-            # versions = versions_all[1:-5]
-            # This would change list from:
-            # ["Latest", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-            # To:
-            # [1, 2, 3, 4, 5, 6, 7, 8]
-            versions_all = client[0].list_versions_by_function(FunctionName=function['FunctionArn'])['Versions']
-            versions = versions_all[1:-int(event['num_versions'])]
-
-            for version in versions:
-                if version['Version'] != function['Version']:
-                    arn = version['FunctionArn']
-                    logger.info('FunctionName={}'.format(arn))
-                    logger.info("Memory size of this version: " + str(version['MemorySize']) + " MB\n")
-
-                    if event['read_only']:
-                        break
-                    else:
-                        memory_free += remove_version(version, client[0])
-                else:
-                    logger.info("Ignoring Latest version for function: " + function['Version'])
-
-        logger.info("Total memory freed: " + str(memory_free) + "MB\n\t In usage: " + str(
-            int(int(client[0].get_account_settings()['AccountUsage']['TotalCodeSize'])/1024))+ "MB")
-        return "Review Function completed!"
-    else:
-        return "No function review in that region!"
+        if len(functions) != 0:
+            logger.info('{}: Functions found, checking for versions'.format(region))
+            for function in functions:
+                memory_free += read_versions_from_funtion(function, event['num_versions'], client[0],region)
+            in_use_memory = int(int(client[0].get_account_settings()['AccountUsage']['TotalCodeSize'])/1024)
+            logger.info("{}: Total memory freed: {} MB and in usage: {} MB".format(region, str(memory_free), str(in_use_memory)))
+            put_metrics_cloudwatch(client[1], in_use_memory)
+            message.append("{}: Review Function completed!".format(region))
+        else:
+            message.append("{}: No function review in that region!".format(region))
+    return message
