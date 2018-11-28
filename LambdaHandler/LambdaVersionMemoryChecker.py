178b5c38624969c5dcae1dff63c13e0ff40edaef
Wendler Zacariotto <zwendler@amazon.com>
2018-11-28 08:31:38 +0000

    Removing the metric creator for the version checker and creating a new script to put metrics to cloudwatch

--- LambdaHandler/LambdaVersionMemoryChecker.py
+++ LambdaHandler/LambdaVersionMemoryChecker.py
@@ -1,16 +1,13 @@
-from __future__ import absolute_import, print_function, unicode_literals
 import logging
-import datetime;
 import boto3
 
 # Initialize logger and set log level
 logger = logging.getLogger()
-logger.setLevel(logging.ERROR)
+logger.setLevel(logging.INFO)
 
 # Initialize Lambda and CloudWatch client for the specific region
 def initialize_services(region):
-    session = boto3.Session(region_name=region)
-    return [session.client('lambda'), session.client('cloudwatch')]
+    return boto3.client('lambda', region_name=region)
 
 # Read all the verions from the Functions in that region
 # versions = versions_all[1:-5]
@@ -19,7 +16,6 @@ def initialize_services(region):
 # To:
 # [1, 2, 3, 4, 5, 6, 7, 8]
 def read_versions_from_funtion(function, event, client_lambda, region):
-    memory_free = 0
     versions_all = client_lambda.list_versions_by_function(FunctionName=function['FunctionArn'])['Versions']
     versions = versions_all[1:-int(event.get('num_versions', 1))]
 
@@ -27,59 +23,44 @@ def read_versions_from_funtion(function, event, client_lambda, region):
     for version in versions:
         if version['Version'] != function['Version']:
             arn = version['FunctionArn']
-            logger.info('{}: FunctionName={}'.format(arn))
-            logger.info('{}: Memory size of this version: {} MB\n'.format(region, version['MemorySize']))
+            logger.info('{}: FunctionName={}'.format(region, arn))
+            logger.info('{}: Memory size of this version: {} MB\n'.format(region, int(version['CodeSize'])/1024))
 
-            if not event.get('read_only', True): memory_free += remove_version(version, client_lambda, region)
+            if not event.get('read_only', True): remove_version(version, client_lambda, region)
         else:
-            logger.info("{}: Ignoring Latest version for function: {}".format(region, function['Version']))
-    return memory_free
+            logger.info('{}: Ignoring Latest version for function: {}'.format(region, function['Version']))
 
-
-# Create CloudWatch Metric for Code Storage usage
-def put_metrics_cloudwatch(client_cloudwatch, in_use_memory):
-    client_cloudwatch.put_metric_data(
-        Namespace='AWS/Lambda',
-        MetricData=[
-            {
-                'MetricName': 'UsedCodeStorage',
-                'Timestamp': datetime.datetime.now().timestamp(),
-                'Value': in_use_memory/1024,
-                'Unit': 'Gigabytes',
-                'StorageResolution': 60
-            },
-        ]
-    )
-
-# Delete the version and return the total of memory freed
+# Delete the version and catch an exception if failed to be deleted.
+# Function with alias cannot be deleted by this code.
 def remove_version(version, client, region):
     try:
         client.delete_function(FunctionName=version['FunctionArn'])
-        logger.info("{}: {} Delete Successful!".format(region, version['FunctionArn']))
-        logger.info("{}: Memory freed {}".format(region, str(version['MemorySize'])))
-        return int(version['MemorySize'])
+        logger.info('{}: {} Delete Successful!'.format(region, version['FunctionArn']))
     except Exception as e:
         logger.error('{}: Failed to delete: {}'.format(region, str(e)))
-        return 0
 
-def clean_old_lambda_versions(event, context):
+def lambda_handler(event, context):
     message = []
-    ec2_regions = [region['RegionName'] for region in boto3.client('ec2').describe_regions()['Regions']]
-    for region in ec2_regions:
+    regions = []
+    regions = [region['RegionName'] for region in boto3.client('ec2').describe_regions()['Regions']]
+    # Check for region in the events
+    if event.get('region', ''):
+        if event['region'] not in regions: return 'Invalid Region'
+        regions = [event['region']]
+    for region in regions:
         # ap-northeast-3 requires special credentials
         if region == 'ap-northeast-3': continue
         client = initialize_services(region)
-        functions = client[0].list_functions()['Functions']
-        memory_usage = int(client[0].get_account_settings()['AccountUsage']['TotalCodeSize'])
+        functions = client.list_functions()['Functions']
+        memory_usage = int(client.get_account_settings()['AccountUsage']['TotalCodeSize'])
         memory_free = 0
         if len(functions) != 0:
-            logger.info('{}: Functions found, checking for versions'.format(region))
+            logger.info('{}: Found {} Functions, checking for versions'.format(region, len(functions)))
             for function in functions:
-                memory_free += read_versions_from_funtion(function, event, client[0], region)
-            in_use_memory = int(int(client[0].get_account_settings()['AccountUsage']['TotalCodeSize'])/1024)
-            logger.info("{}: Total memory freed: {} MB and in usage: {} MB".format(region, str(memory_free), str(in_use_memory)))
-            put_metrics_cloudwatch(client[1], in_use_memory)
-            message.append("{}: Review Function completed!".format(region))
+                read_versions_from_funtion(function, event, client, region)
+            memory_after_prunning = client.get_account_settings()['AccountUsage']['TotalCodeSize']/1024
+            logger.info('{}: Total memory freed: {} MB and in usage: {} MB'.format(region, str(memory_usage/1024 - memory_after_prunning), memory_after_prunning))
+            message.append('{}: Review Function completed!'.format(region))
         else:
-            message.append("{}: No function review in that region!".format(region))
+            message.append('{}: No function review in that region!'.format(region))
     return message
